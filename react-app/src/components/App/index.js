f99d29eaa2b729e687d0cfd2bce7d7ab2c83e50a
Dimitri M <ibmiti@gmail.com>
2020-11-13 11:34:34 -0500

    importing fetch to application, using fetch to fetch api data using data to setState

---
+++ react-app/src/components/App/index.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import Intro from '../intro';
+import './App.css';
+import 'whatwg-fetch';
+
+class App extends Component {
+state = {
+  series : []
+}
+
+componentDidMount(){
+  fetch('http://api.tvmaze.com/search/shows?q=Vikings')
+  .then(response => response.json())
+  .then(json => this.setState({'series': json}))
+}
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Tv Series List</h1>  
+        </header>
+        <Intro message="Here you can find all of your most loved series" />
+        The length of series array - {this.state.series.length}
+      </div>
+    );
+  }
+}
+
+export default App;
