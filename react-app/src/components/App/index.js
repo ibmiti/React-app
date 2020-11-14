1c37cb641b4fc6151fb08ba5be303add16918750
Dimitri M <ibmiti@gmail.com>
2020-11-14 06:43:20 -0500

    sending readme and current WIP

--- react-app/src/components/App/index.js
+++ react-app/src/components/App/index.js
@@ -1,19 +1,10 @@
 import React, { Component } from 'react';
 import Intro from '../intro';
+import Series from '../../containers/series';
 import './App.css';
 import 'whatwg-fetch';
 
 class App extends Component {
-state = {
-  series : []
-}
-
-componentDidMount(){
-  fetch('http://api.tvmaze.com/search/shows?q=Vikings')
-  .then(response => response.json())
-  .then(json => this.setState({'series': json}))
-}
-
   render() {
     return (
       <div className="App">
@@ -21,7 +12,7 @@ componentDidMount(){
           <h1 className="App-title">Tv Series List</h1>  
         </header>
         <Intro message="Here you can find all of your most loved series" />
-        The length of series array - {this.state.series.length}
+        <Series />
       </div>
     );
   }
